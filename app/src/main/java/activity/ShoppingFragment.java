b989fe5d34f0cfe4c330eb45dcb3af6c0fe78faa
zhayue <zhayue@corp.netease.com>
2018-07-20 16:00:44 +0800

    test

---
+++ app/src/main/java/activity/ShoppingFragment.java
@@ -0,0 +1,41 @@
+package activity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cherrycha.material_design.R;
+
+
+public class ShoppingFragment extends Fragment {
+
+    public ShoppingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_shopping, container, false);
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
